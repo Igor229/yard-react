f35125cb3ec586d884748bb56a63c09c5bf42c64
Igor229 <frunza2029@gmail.com>
2023-07-20 17:19:53 +0300

    main-bg video test

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import './About.scss'
+import MainVideoBG from '../assets/videos/main-video.MP4'
+import MainVideoBGQuality from '../assets/videos/main-video_better-quality.MP4'
 
 function About () {
     return (
         <section className="about" id="main-container">
-        <div className="about__content">
+            <div className='video-container'>
+                <video src={MainVideoBGQuality} typeof='video/mp4' className='main-video' autoPlay loop muted/>
+            </div>
+        {/* <div className="about__content">
             <h1 className="about__content-title">YARD DEVELOPMENT</h1>
             <p className="about__content-text">
                 Понад 89 будинків введено в експлуатацію. Дбаємо <br/> про ваш комфорт та затишок!
             </p>
-        </div>
+        </div> */}
     </section>
     )
 }
